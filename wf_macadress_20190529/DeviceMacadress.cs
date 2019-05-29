a1203f0100d36be587fff515981f3e1036cf941d
david <2657070472@qq.com>
2019-05-29 15:24:35 +0800

    macadress

---
+++ wf_macadress_20190529/DeviceMacadress.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+using System.Management;
+using System.Net;
+using Microsoft.Win32;
+using System.IO;
+
+namespace wf_macadress_20190529
+{
+    public class GetMAC
+    {
+        public string GetMACAddress()
+        {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            String sMacAddress = string.Empty;
+            foreach (NetworkInterface adapter in nics)
+            {
+                if (sMacAddress == String.Empty)// only return MAC Address from first card  
+                {
+                    IPInterfaceProperties properties = adapter.GetIPProperties();
+                    sMacAddress = adapter.GetPhysicalAddress().ToString();
+                }
+            }
+            return sMacAddress;
+        }
+
+        public string GetNetMACAddress()
+        {
+            string netmac = "";
+            foreach (NetworkInterface netInt in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                netmac = netInt.GetPhysicalAddress().ToString();
+
+            }
+            return netmac;
+        }
+    }
+
+ 
+}
--- wf_macadress_20190529/Form1.cs
+++ wf_macadress_20190529/Form1.cs
@@ -11,9 +11,26 @@ namespace wf_macadress_20190529
 {
     public partial class Form1 : Form
     {
+        public MoreMethodGetMAC adress;
+        public GetMAC deviceMac;
         public Form1()
         {
             InitializeComponent();
+            adress = new MoreMethodGetMAC();
+            deviceMac = new GetMAC();
+        }
+
+     
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string macadress1 = "";
+            string macadress2 = "";
+            string macadress3 = "";
+            macadress1 = adress.GetMacAddress();
+
+            macadress2 = deviceMac.GetMACAddress();
+            macadress3 = deviceMac.GetNetMACAddress();
+            richTextBox1.Text += macadress1 +"    " + macadress2 + "    " + macadress3 + "    ";
         }
     }
 }
